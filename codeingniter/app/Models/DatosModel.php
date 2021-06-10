cceb45773bb66d2e763015c0cefe2a71f11c5ce5
diberoberto12 <83965084+diberoberto12@users.noreply.github.com>
2021-06-10 13:59:56 -0500

    Create DatosModel.php

---
+++ codeingniter/app/Models/DatosModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class DatosModel extends Model{
+    public function listar(){
+        $m = $this->db->query("SELECT * from persona");
+        return $m->getResult();
+    }
+}
