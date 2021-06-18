f9a4e4a567438745bd9b0dcd48fe484e77400f6d
diberoberto12 <83965084+diberoberto12@users.noreply.github.com>
2021-06-18 13:52:49 -0500

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -44,7 +44,7 @@
                  <th><?php echo $dato->nombre ?></th>
                  <th><?php echo $dato->apellido_paterno ?></th>
                  <th><?php echo $dato->apellido_materno ?></th>
-                 <th><a href="<?php echo base_url('/index.php/obtenerInformacion').'/'.$dato->id ?>" class="btn btn-sm btn-warning">Editar</a></h>
+                 <th><a href="<?php echo base_url('/index.php/obtenerInformacion').'/'.$dato->id ?>" class="btn btn-sm btn-warning">Editar</a></th>
                  <th><a href="<?php echo base_url('/index.php/eliminar').'/'.$dato->id ?>" class="btn btn-sm btn-danger">Eliminar</a></th>
             
              </tr>
